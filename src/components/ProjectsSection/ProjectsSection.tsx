9bedbee0a0b77ef09bed4e25a62810f3ba7c02d8
Diego Rojas <rrojas1296@gmail.com>
2026-04-10 15:49:51 -0500

    master: Added new project

--- src/components/ProjectsSection/ProjectsSection.tsx
+++ src/components/ProjectsSection/ProjectsSection.tsx
@@ -11,12 +11,12 @@ const ProjectsSection = () => {
     <div id="projects" className="mt-35 lg:mt-55 w-10/12 max-w-7xl mx-auto">
       <h1 className="font-bold text-3xl text-text-1">{t("title")}</h1>
       <h2 className="text-text-2 text-sm mt-1">{t("subtitle")}</h2>
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 mt-10">
+      <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-5 mt-10">
         {projects.map(({ id, name, urlImage, description, stack, buttons }) => {
           return (
             <div
               key={id}
-              className="p-3 border border-border-2 rounded-lg"
+              className="p-3 border border-border-2 rounded-lg flex flex-col"
               style={{
                 backgroundImage: "var(--bg-linear)",
               }}
@@ -31,7 +31,11 @@ const ProjectsSection = () => {
               <h1 className="text-text-1 text-lg font-medium mt-3">
                 {t(name)}
               </h1>
-              <p className="text-sm text-text-2 mt-1">{t(description)}</p>
+              <p className="text-sm text-text-2 mt-1">
+                {t(description).length > 130
+                  ? t(description).slice(0, 130) + "..."
+                  : t(description)}
+              </p>
               <div className="mt-3 flex flex-wrap gap-3">
                 {stack.map((s) => {
                   const skill = skills.find((sk) => sk.label === s);
@@ -52,6 +56,7 @@ const ProjectsSection = () => {
                   );
                 })}
               </div>
+              <div className="flex-1" />
               <div className="grid grid-cols-1 md:grid-cols-2 gap-4 mt-3 font-medium">
                 {buttons.map(({ icon: Icon, id, buttonType, url, label }) => {
                   return (
--- src/constants/projects.tsx
+++ src/constants/projects.tsx
@@ -1,3 +1,4 @@
+import FigmaIcon from "@/components/Icons/FigmaIcon";
 import GithubIcon from "@/components/Icons/GithubIcon";
 import { IProject } from "@/types/Projects";
 import { GlobeIcon } from "lucide-react";
@@ -23,6 +24,30 @@ export const projects: IProject[] = [
         label: "projects.buttons.github",
         icon: GithubIcon,
         url: "https://github.com/rrojas1296/nvim-2026",
+        buttonType: "secondary",
+      },
+    ],
+  },
+  {
+    id: "2",
+    name: "projects.devflow.name",
+    urlImage:
+      "https://res.cloudinary.com/dxkd8sigp/image/upload/v1775853412/20260410_15h36m06s_grim_izpai6.png",
+    description: "projects.devflow.description",
+    stack: ["React", "Typescript", "Zustand", "NodeJS", "Next.js"],
+    buttons: [
+      {
+        id: "1",
+        label: "projects.buttons.figma",
+        icon: FigmaIcon,
+        url: "https://www.figma.com/design/KKjm6eY2OABmVYy9z7EKWk/Devflow?node-id=0-1&t=nK1NHJIGKOFYLUuS-1",
+        buttonType: "filled",
+      },
+      {
+        id: "2",
+        label: "projects.buttons.github",
+        icon: GithubIcon,
+        url: "https://github.com/rrojas1296/devflow-client",
         buttonType: "secondary",
       },
     ],
